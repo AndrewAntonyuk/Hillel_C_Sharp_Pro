c9520025797a1c5ee9a2be0f9f6ed81ca30135a8
Andrew Antonyuk <antonyuk.andrij@gmail.com>
2023-07-30 21:58:23 +0300

    Add Lesson 1: Intro into classes. Calculator class

---
+++ Homework/Lesson_1_IntroClass_Calculator/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1_IntroClass_Calculator
+{
+    public class Calculator
+    {
+        public dynamic sum(dynamic add1, dynamic add2)
+        {
+            return add1 + add2;
+        }
+
+        public dynamic dif(dynamic minuend, dynamic subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+
+        public dynamic mul(dynamic multiplicand, dynamic multiplier)
+        {
+            return multiplicand * multiplier;
+        }
+
+        public dynamic div(dynamic dividend, dynamic divisor)
+        {
+            return dividend / divisor;
+        }
+
+        public dynamic perc(dynamic percentage, dynamic total)
+        {
+            return percentage / 100.0 * total;
+        }
+
+        public dynamic sqrt(dynamic val)
+        {
+            return Math.Sqrt(val);
+        }
+    }
+}
---
+++ Homework/Lesson_1_IntroClass_Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1_IntroClass_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            double testVar1 = 10.0;
+            double testVar2 = 80.0;
+
+            #region test of sum
+            Console.WriteLine("Sum {0} + {1}: {2}", testVar1, testVar2, calculator.sum(testVar1, testVar2));
+            Console.WriteLine();
+            #endregion
+
+            #region test of difference
+            Console.WriteLine("Difference {0} - {1}: {2}", testVar1, testVar2, calculator.dif(testVar1, testVar2));
+            Console.WriteLine();
+            #endregion
+
+            #region test of multiplication
+            Console.WriteLine("Multiplication {0} * {1}: {2}", testVar1, testVar2, calculator.mul(testVar1, testVar2));
+            Console.WriteLine();
+            #endregion
+
+            #region test of division
+            Console.WriteLine("Division {0} / {1}: {2}", testVar1, testVar2, calculator.div(testVar1, testVar2));
+            Console.WriteLine();
+            #endregion
+
+            #region test of percentage
+            Console.WriteLine("Percentage {0}% from {1}: {2}", testVar1, testVar2, calculator.perc(testVar1, testVar2));
+            Console.WriteLine();
+            #endregion
+
+            #region test of square root
+            Console.WriteLine("Square root from {0}: {1}", testVar1, calculator.sqrt(testVar1));
+            Console.WriteLine();
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
+
